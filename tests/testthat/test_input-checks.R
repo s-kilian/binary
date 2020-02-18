0c654877e1e71b3b866a17ea9363316105f2526d
Samuel Kilian <kilian@imbi.uni-heidelberg.de>
2020-02-18 10:10:09 +0100

    Test functions for input checks.

---
+++ tests/testthat/test_input-checks.R
@@ -0,0 +1,19 @@
+context("Test of input check functions")
+
+library(tidyverse)
+
+## Check (0, 1) ################################################################
+test_that("Interval check gives appropriate errors", {
+  expect_error(check.0.1(c(0.5, 1), "errmess"), "errmess")
+  expect_error(check.0.1(c(0.5, 0), "errmess"), "errmess")
+  expect_error(check.0.1(c(0.5, -1), "errmess"), "errmess")
+  check.0.1(c(0.1, 0.3, 0.99), "error")
+})
+
+## Check positive integer ######################################################
+test_that("Positive integer check gives appropriate errors", {
+  expect_error(check.pos.int(c(0, 1, 2), "errmess"), "errmess")
+  expect_error(check.pos.int(c(-1, 1, 2), "errmess"), "errmess")
+  expect_error(check.pos.int(c(1, 2, 3.7), "errmess"), "errmess")
+  check.pos.int(c(1, 2, 10))
+})
