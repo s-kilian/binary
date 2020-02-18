ed895c38718132f982cce5bafc6f32e8b871720f
Samuel Kilian <kilian@imbi.uni-heidelberg.de>
2020-02-18 09:15:59 +0100

    Moved input checks to extra file.

--- R/boschloo-functions.R
+++ R/boschloo-functions.R
@@ -29,30 +29,6 @@
 ##..............................................................................
 
 
-## Functions ###################################################################
-## Checks       ################################################################
-check.0.1 <- function(
-  values,           # vector of values that shall lie in the interval (0, 1)
-  message           # error message to display if one of the values doesn't lie
-                    # in (0,1)
-){
-  # Check whether values all lie in interval (0, 1) and output error message if not
-  if (any(values <= 0 | values >= 1)) {
-    stop(message)
-  }
-}
-
-check.pos.int <- function(
-  values,           # vector of values that shall be positive integers
-  message           # error message to display if one of the values is not a
-                    # positive integer
-){
-  # Check whether values all are positive integers and output error message if not
-  if (any(values %% 1 != 0 | values <= 0)) {
-    stop(message)
-  }
-}
-
 ## Superiority #################################################################
 # Test problem:
 # H_0: p_E <= p_C
