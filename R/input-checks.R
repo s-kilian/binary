c8a076c089061a88218fea35b7ced1b2e877e780
s-kilian <kilian@imbi.uni-heidelberg.de>
2021-04-23 15:20:39 +0200

    - added function to check whether delta is in correct range - added function to default set delta if it is not specified

--- R/input-checks.R
+++ R/input-checks.R
@@ -33,3 +33,39 @@ check.pos.int <- function(
     stop(message)
   }
 }
+
+check.delta.method.better <- function(
+  delta,
+  method,
+  better
+){
+  # Check whether delta fits the selected method
+  if(method == "RD"){
+    if(abs(delta) >= 1) stop("The NI-margin for risk difference has to be in (-1, 1).")
+    if(better == "high" & delta > 0) warning("If better == \"high\", the NI-margin is usually not greater than 0.")
+    if(better == "low" & delta < 0) warning("If better == \"low\", the NI-margin is usually not smaller than 0.")
+  }
+  # Check whether delta fits specification of better
+  if(method %in% c("RR", "OR")){
+    if(delta <= 0) stop("The NI-margin for risk ratio and odds ratio has to be positive.")
+    if(better == "high" & delta > 1) warning("If better == \"high\", the NI-margin is usually not greater than 1.")
+    if(better == "low" & delta < 1) warning("If better == \"low\", the NI-margin is usually not smaller than 1.")
+  }
+}
+
+check.delta.null <- function(
+  method,
+  delta
+){
+  if(is.null(delta)){
+    if(method == "RD"){
+      warning("delta not specified and set to 0 (superiority test regarding risk difference).")
+      delta <- 0
+    }
+    if(method %in% c("RR", "OR")){
+      warning("delta not specified and set to 1 (superiority test regarding risk ratio or odds ratio).")
+      delta <- 1
+    }
+  }
+  return(delta)
+}
