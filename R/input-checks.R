5a8de7202bda2d2b8ca27cc6c02580e0ccb9a16f
Kilian <fo371@uni-heidelberg.de>
2022-07-07 14:50:36 +0200

    Adapted input checks to separation of efect measure and test statistic.

--- R/input-checks.R
+++ R/input-checks.R
@@ -5,7 +5,7 @@
 ##                Output: Functions:
 ##                        check.0.1: Value lies in open interval (0, 1)?
 ##                        check.pos.int: Value is positive integer?
-##                        check.delta.method.better: Delta fits method and better?
+##                        check.delta.eff_meas.better: Delta fits eff_meas and better?
 ##                        check.delta.null: Default delta value
 ##                        check.effect.delta.better: Effect fits delta and better?
 ##      Date of creation: 2020-02-18
@@ -45,19 +45,19 @@ check.pos.int <- function(
   }
 }
 
-check.delta.method.better <- function(
+check.delta.eff_meas.better <- function(
   delta,
-  method,
+  eff_meas,
   better
 ){
-  # Check whether delta fits the selected method
-  if(method == "RD"){
+  # Check whether delta fits the selected eff_meas
+  if(eff_meas == "RD"){
     if(abs(delta) >= 1) stop("The NI-margin for risk difference has to be in (-1, 1).")
     if(better == "high" & delta > 0) warning("If better == \"high\", the NI-margin is usually not greater than 0.")
     if(better == "low" & delta < 0) warning("If better == \"low\", the NI-margin is usually not smaller than 0.")
   }
   # Check whether delta fits specification of better
-  if(method %in% c("RR", "OR")){
+  if(eff_meas %in% c("RR", "OR")){
     if(delta <= 0) stop("The NI-margin for risk ratio and odds ratio has to be positive.")
     if(better == "high" & delta > 1) warning("If better == \"high\", the NI-margin is usually not greater than 1.")
     if(better == "low" & delta < 1) warning("If better == \"low\", the NI-margin is usually not smaller than 1.")
@@ -65,15 +65,15 @@ check.delta.method.better <- function(
 }
 
 check.delta.null <- function(
-  method,
+  eff_meas,
   delta
 ){
   if(is.null(delta)){
-    if(method == "RD"){
+    if(eff_meas == "RD"){
       warning("delta not specified and set to 0 (superiority test regarding risk difference).")
       delta <- 0
     }
-    if(method %in% c("RR", "OR")){
+    if(eff_meas %in% c("RR", "OR")){
       warning("delta not specified and set to 1 (superiority test regarding risk ratio or odds ratio).")
       delta <- 1
     }
@@ -84,14 +84,14 @@ check.delta.null <- function(
 check.effect.delta.better <- function(
   p_E,
   p_C,
-  method,
+  eff_meas,
   delta,
   better
 ){
   # check if specified prob. lie in region of alternative hyp.
-  effect <- effect(p_E = p_E, p_C = p_C, method = method)
+  effect <- effect(p_E = p_E, p_C = p_C, eff_meas = eff_meas)
   if(better == "high") fits.alt <- effect > delta
   if(better == "low") fits.alt <- effect < delta
-  if(!fits.alt) stop(paste0("delta = ", delta, " is ", better, "er than assumed effect ", method, " = ", effect))
+  if(!fits.alt) stop(paste0("delta = ", delta, " is ", better, "er than assumed effect ", eff_meas, " = ", effect))
 }
 
